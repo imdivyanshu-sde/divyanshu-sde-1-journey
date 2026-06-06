9256855ac32029e3b284d61e1d6ebb0f2f4f67ff
gopi-devops-cloud <gopibhatt728@gmail.com>
2026-06-06 18:35:53 +0530

    Add java

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 // First Java Program
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World2!");
+        System.out.print("Hello, World3!\n");
+        System.out.println("Hello, World3!");
+        System.out.print("Hello, World3!");
     }
 }
